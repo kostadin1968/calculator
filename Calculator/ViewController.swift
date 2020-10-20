997497c092a79b4a56e1d97e5763f132bb4835ac
kostadin dimitrov polukchiev <koce1968@gmail.com>
2020-10-20 22:50:08 +0300

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by kostadin dimitrov polukchiev on 20.10.20.
+//  Copyright © 2020 com.kostadin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
